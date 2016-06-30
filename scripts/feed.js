63ef37c8346efa912754fb66ede3551e408ba7b7
Blake <btickell@gmail.com>
2016-06-30 06:02:21 -0700

    file restructuring

--- scripts/feed.js
+++ scripts/feed.js
@@ -1,19 +1,30 @@
+//localStorage.clear();
+updateUL();
 
-
-var posts = [];
-
-class Post {
-	constructor(userInput) {
-		this.text = userInput;
-	};	
+function Post(userInput) {
+	
+	this.text = userInput;	
 };
 
 var newPost = function() {
 	var text = document.getElementById("textfield1").value;
 	var post = new Post(text);
 	console.log(post);
-	posts.push(post);
-	console.log(posts);
+	localStorage.setItem(JSON.stringify(localStorage.length), JSON.stringify(post.text));
+	console.log("got here!");
 };	
 
 document.forms["input"].onsubmit = newPost;
+
+function updateUL() {
+	var list = document.createElement('ul');
+
+	for (i = localStorage.length-1; i >= 0; i -= 1) {
+		var item = document.createElement('li');
+		var content = localStorage.getItem(JSON.stringify(i));
+		item.appendChild(document.createTextNode(content));
+		list.appendChild(item);
+	}
+
+	document.getElementById('feedlist').appendChild(list);
+}
